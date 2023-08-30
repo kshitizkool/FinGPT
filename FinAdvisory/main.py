8b21adf2226580a40a0dab7ef953e67705006a5f
KSHITIZ KUMAR <87076914+kshitizkool@users.noreply.github.com>
2023-08-30 18:48:34 +0530

    Update main.py

--- FinAdvisory/main.py
+++ FinAdvisory/main.py
@@ -2,12 +2,13 @@ import streamlit as st
 import helper
 from fetchstockinfo import Anazlyze_stock
 import openai
+from secret_key import set_api_key
 st.title("🦜🔗Financial Advisory-GPT")
-st.write("This bot scraps and gathers real time stock related information using OpenAI & Yahoo Finance and analyzes it using LLM and Langchain")
+st.write("This bot scraps and gathers real time stock related information and analyzes it using LLM and Langchain")
 
 query = st.text_input('Input the Company name to be advised')
 
-Enter=st.button("Analyze")
+Enter = st.button("Analyze")
 
 if Enter:
     import time
@@ -25,9 +26,11 @@ if Enter:
 #         st.write("**Investment Plans**")
 #         for item in menu_items:
 #             st.write("-", item)
-
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    openapi_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    if st.button("Save API Key"):
+        set_api_key(openapi_key)
+        st.success("API key saved!")
 st.title("💬 Chatbot")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Hey!, How can I help you?"}]
@@ -36,14 +39,11 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
-
-    openai.api_key = openai_api_key
+    openai.api_key = openapi_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
     st.chat_message("assistant").write(msg.content)
+
