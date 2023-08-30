7f2172a0cf38c019a8edbf2c13dcf77202d81764
KSHITIZ KUMAR <87076914+kshitizkool@users.noreply.github.com>
2023-08-30 18:49:24 +0530

    Update secret_key.py

--- FinAdvisory/secret_key.py
+++ FinAdvisory/secret_key.py
@@ -1,2 +1,10 @@
-openapi_key = "YOUR_OPENAI_API_KEY"
-serpapi_key = "YOUR_SERPAPI_KEY"
+API_KEY = None
+
+
+def set_api_key(openapi_key):
+    global API_KEY
+    API_KEY = openapi_key
+
+
+def get_api_key():
+    return API_KEY
